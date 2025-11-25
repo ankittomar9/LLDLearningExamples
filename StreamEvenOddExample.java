90a114f6f4f1931310ce01aa824dec3038369f76
tomar <tomarankit78@gmail.com>
2025-11-25 22:44:31 +0530

    Stream API Learning Examples

---
+++ StreamEvenOddExample.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamEvenOddExample {
+    public static void main(String args[]){
+        List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        List<Integer>EvenNumbers=numbers.stream()
+        .filter(n->n%2==0)
+        .collect(Collectors.toList());
+        
+
+        System.out.println("Original numbers: "+numbers);
+          System.out.println("EvenODD numbers: "+EvenNumbers);
+          
+
+    }
+    
+}
