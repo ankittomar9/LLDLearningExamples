86c4ff56ce2e929c3b5ffde6efa51896f387ce16
tomar <tomarankit78@gmail.com>
2025-12-09 16:58:59 +0530

    New learning Examples

--- JavaCollections/Map/HashMapPrint.java
+++ JavaCollections/Map/HashMapPrint.java
@@ -1,5 +1,19 @@
 package JavaCollections.Map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapPrint {
+    public static void main(String args[]){
+        Map<Integer,Integer>hms=new HashMap<>();
+        hms.put(1,10);
+         hms.put(2,20);
+        hms.put(3,30);
+        hms.put(4,40);
+
+        System.out.println("HashMap Using Map : "+hms );
+
+
+    }
     
 }
