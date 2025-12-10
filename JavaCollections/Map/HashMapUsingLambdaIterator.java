9f5d2709ef734f5c879ce7a7fbc20bd1d4e9b611
tomar <tomarankit78@gmail.com>
2025-12-10 11:23:31 +0530

    New learning Examples

---
+++ JavaCollections/Map/HashMapUsingLambdaIterator.java
@@ -0,0 +1,12 @@
+package JavaCollections.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapUsingLambdaIterator {
+    public static void main(String args[]){
+        Map<Integer,Integer> map=new HashMap<>();
+        
+        
+    }    
+}
