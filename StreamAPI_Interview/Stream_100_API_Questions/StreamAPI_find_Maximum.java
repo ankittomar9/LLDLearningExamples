3b152c1735bec73a431f1b10e22ab9e7a5324fc0
tomar <tomarankit78@gmail.com>
2025-12-12 16:01:50 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/StreamAPI_find_Maximum.java
@@ -0,0 +1,25 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamAPI_find_Maximum {
+    public static void main(String args[]){
+        
+        List<Integer>list=Arrays.asList(10,45,1,6,80,4,30);
+
+        int maxNumber=list.stream()
+        .max(Integer::compare)
+        .orElseThrow();
+
+        System.out.println(" max number in a given List : " +maxNumber);
+
+
+
+
+
+    }
+
+    
+}
