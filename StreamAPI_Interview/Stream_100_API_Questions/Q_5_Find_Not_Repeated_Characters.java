caabb0123485710837bf035a4534496bc7892f5b
tomar <tomarankit78@gmail.com>
2025-12-12 19:06:14 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_5_Find_Not_Repeated_Characters.java
@@ -0,0 +1,18 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Optional;
+
+public class Q_5_Find_Not_Repeated_Characters {
+    public static void main(String args[]){
+        String s1= "Bookkeeper";
+
+        Optional<Character> firstNotRepeated=s1.chars()
+        .mapToObj(ch->(char)ch )
+        .filter(ch-> s1.indexOf(ch)==s1.lastIndexOf(ch))
+        .findFirst();
+
+        System.out.println(" Last Non repeating char : "+firstNotRepeated);
+
+    }
+    
+}
