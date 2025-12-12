8d0cc1a7c09c94d2499c6a6e386c6f667a9a7316
tomar <tomarankit78@gmail.com>
2025-12-12 15:48:45 +0530

    New learning Examples

---
+++ StreamAPI_Interview/StreamAPIEvenOddRevision.java
@@ -0,0 +1,26 @@
+package StreamAPI_Interview;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamAPIEvenOddRevision {
+    public static void main(String args[]){
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8, 9);
+        List<Integer> EvenList=list.stream()
+        .filter(n->n%2==0)
+        .collect(Collectors.toList());
+        System.out.println("EvenList : "+EvenList);
+
+
+        List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7,8, 9);
+        List<Integer> OddList=list1.stream()
+        .filter(n->n%2!=0)
+        .collect(Collectors.toList());
+        System.out.println("EvenList : "+OddList);
+
+
+    }
+    
+}
