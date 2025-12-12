99da87e05cb58df581751ceda1cd460e7bb6482e
tomar <tomarankit78@gmail.com>
2025-12-12 18:40:51 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/StreamAPI_Find_last_Word_in_String.java
@@ -0,0 +1,27 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class StreamAPI_Find_last_Word_in_String {
+    public static void main(String args[]){
+        //Find last Word in String
+        String s1="I am a engineer software";
+        String[] words=s1.split(" ");
+
+        System.out.println(s1);
+        System.out.println(Arrays.toString(words));
+        
+        // Optional Optional<String> lastWordOptional=stream.words   <- why not this
+        Optional<String> lastWordOptional=Arrays.stream(words)
+        .reduce((first,second)->second);                     // what is happening here
+
+        String lastword=lastWordOptional.orElse("String is empty");
+        System.out.println("The Last Word is: " + lastword);
+
+
+
+
+    }
+    
+}
