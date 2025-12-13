ff2f5582eb8dbf4f56a19ac43a042054d1413aa4
tomar <tomarankit78@gmail.com>
2025-12-13 19:25:16 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_9_Find_if_any_string_matches_in_list.java
@@ -0,0 +1,24 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q_9_Find_if_any_string_matches_in_list {
+    public static void main(String args[]){
+        List<String> list=Arrays.asList("api","why",
+        "learning api","just chill","build api");
+
+        List<String> matchList=list.stream()
+        .filter(s->s.contains("api"))
+        .collect(Collectors.toList());
+
+        System.out.println("Filtered api \n"+matchList);
+
+        
+
+
+
+    }
+}
+
