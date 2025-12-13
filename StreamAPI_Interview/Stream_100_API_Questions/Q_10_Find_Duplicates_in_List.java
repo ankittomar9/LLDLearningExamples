6cd310a9051c40e5ed57827a29a2b39a3f9ef01d
tomar <tomarankit78@gmail.com>
2025-12-13 23:40:49 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_10_Find_Duplicates_in_List.java
@@ -0,0 +1,24 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Q_10_Find_Duplicates_in_List {
+    public static void main(String args[]){
+     List<Integer> list=Arrays.asList(1,1,2,3,3,3,4,5,5,6,8,7,8,8,9,1);
+
+       Set<Integer> uniqueElements =new HashSet<>();
+
+       Set<Integer> duplicates=list.stream() // pipeline open
+       .filter(n -> !uniqueElements.add(n)) // filter based on condition
+       .collect(Collectors.toSet());        // add and store them
+
+        System.out.println("Original List \n"+ list);
+       System.out.println("Unique Elements \n"+ duplicates);
+        
+    }
+    
+}
--- StreamAPI_Interview/Stream_100_API_Questions/Q_6_ConvertAllLetterstoUpperCase.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_6_ConvertAllLetterstoUpperCase.java
@@ -1,15 +1,14 @@
 package StreamAPI_Interview.Stream_100_API_Questions;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Q_6_ConvertAllLetterstoUpperCase {
     public static void main(String args[]){
-
     //Convert List of Strings to Uppercase 
     
-    List<String> words=Arrays.asList("Software","Developer","Learner"," Engineer");
+    List<String> words=Arrays.asList("Software","Developer",
+    "Learner"," Engineer");
 
     List<String> uppercaseWords=words.stream()
     .map(String::toUpperCase)
