8bcc740c319e9fc09441d6e9af7d9406ba6affac
tomar <tomarankit78@gmail.com>
2025-12-14 18:05:08 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_11_faltten_merge_Multiple_List_Into_1.java
@@ -0,0 +1,28 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q_11_faltten_merge_Multiple_List_Into_1 {
+    public static void main(String args[]){
+        //Flatten or Merge a  List of Lists
+
+    List<List<Integer>> list=Arrays.asList(
+        Arrays.asList(1,2,3),            //list1
+          Arrays.asList(7,8,9,10),       //list2
+          Arrays.asList(4,5,6)           //list 3
+          
+    );    
+     System.out.println("Original List \n"+list+"\n");
+
+    List<Integer> merged_Faltten = list.stream()
+    .flatMap(List::stream)
+    .collect(Collectors.toList());
+
+    System.out.println("Merged Flatten List \n"+merged_Faltten); // Merged into single List
+
+
+    }
+    
+}
---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_12_Concatenate_multiple_string_words.java
@@ -0,0 +1,18 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q_12_Concatenate_multiple_string_words {
+    public static void main(String args[]){
+        List<String> words=Arrays.asList("Java","Collections","is","very","powerful","Feature");
+           System.out.println("Old words : \n"+words+"\n");
+
+        String concatenated_all_words=words.stream()
+        .reduce("",(s1,s2)->s1+" "+s2).trim();
+        
+
+        System.out.println("New String : \n"+concatenated_all_words);
+    }
+    
+}
