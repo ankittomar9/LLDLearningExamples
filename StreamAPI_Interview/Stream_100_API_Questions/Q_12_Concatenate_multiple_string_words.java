cc760908f9708ba90dfb38b9a7195b7677751142
tomar <tomarankit78@gmail.com>
2025-12-14 22:44:11 +0530

    New learning Examples

--- StreamAPI_Interview/Stream_100_API_Questions/Q_12_Concatenate_multiple_string_words.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_12_Concatenate_multiple_string_words.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class Q_12_Concatenate_multiple_string_words {
     public static void main(String args[]){
-        List<String> words=Arrays.asList("Java","Collections","is","very","powerful","Feature");
-           System.out.println("Old words : \n"+words+"\n");
+        //Concatenate multiple words into a single string
+        List<String> words=Arrays.asList("Java","Collections","is",
+        "very","powerful","Feature");
+           System.out.println("Original Words \n"+words+"\n");
 
         String concatenated_all_words=words.stream()
         .reduce("",(s1,s2)->s1+" "+s2).trim();
