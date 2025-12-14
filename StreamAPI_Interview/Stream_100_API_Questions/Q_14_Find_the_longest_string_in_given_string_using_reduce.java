fce05db71c7aa2655c874bc4d3cea3b911ccbfc6
tomar <tomarankit78@gmail.com>
2025-12-14 19:16:55 +0530

    New learning Examples

--- StreamAPI_Interview/Stream_100_API_Questions/Q_14_Find_the_longest_string_in_given_string_using_reduce.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_14_Find_the_longest_string_in_given_string_using_reduce.java
@@ -6,10 +6,13 @@ import java.util.List;
 public class Q_14_Find_the_longest_string_in_given_string_using_reduce {
     public static void main(String args[]){
         List<String> list=Arrays.asList("Software", "Developer", "Engineering", "Learning");
-  
-  
-  
-        
+
+        String longest_word=list.stream()
+        .reduce((s1, s2)->s1.length() > s2.length() ? s1:s2)
+        .orElse("List is Empty");
+   
+         System.out.println("The Longest Word is: " + longest_word);
+
   
   
   
