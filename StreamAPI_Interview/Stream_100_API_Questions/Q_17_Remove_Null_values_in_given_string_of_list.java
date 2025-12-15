88d8f08c1458e94ff18e8d6b340ea530def4a8d5
tomar <tomarankit78@gmail.com>
2025-12-15 17:13:07 +0530

    New learning Examples

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_17_Remove_Null_values_in_given_string_of_list.java
@@ -0,0 +1,24 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Q_17_Remove_Null_values_in_given_string_of_list {
+    public static void main(String args[]){
+
+        List<String> words= Arrays.asList("Java",null,"collection",
+                null,"learning",null,"is",null,"fun");
+
+        System.out.println("Original list \n"+words);
+
+        List<String> filteredNulls=words.stream()
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+
+         System.out.println("Removed Nulls \n"+filteredNulls);
+
+    }
+    
+}
