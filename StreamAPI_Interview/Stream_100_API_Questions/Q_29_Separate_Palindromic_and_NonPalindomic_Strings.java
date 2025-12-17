0c0901e1b2700bdeefc4456c2ef68f7f72981fb8
tomar <tomarankit78@gmail.com>
2025-12-17 22:51:18 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_29_Separate_Palindromic_and_NonPalindomic_Strings.java
@@ -0,0 +1,27 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Q_29_Separate_Palindromic_and_NonPalindomic_Strings {
+    public static void main(String args[]){
+        List<String> myStrings=Arrays.asList("naman","aman","nitin"
+        ,"jatin","level","travel"); 
+
+        Map<Boolean,List<String>> SeparationVariable=myStrings.stream()
+        .collect(Collectors.partitioningBy(
+            // this is our palindromic logic check
+            n->n.equals(new StringBuilder(n)
+            .reverse().toString()
+    )));
+
+        System.out.println("Separated palindromes are \n"+ SeparationVariable);
+
+
+
+        
+    }
+    
+}
