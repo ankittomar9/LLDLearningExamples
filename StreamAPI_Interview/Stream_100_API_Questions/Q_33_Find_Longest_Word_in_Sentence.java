8e557cbbd3c67c51e3ce3a809ba3d00a4376af3c
tomar <tomarankit78@gmail.com>
2025-12-18 23:52:07 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_33_Find_Longest_Word_in_Sentence.java
@@ -0,0 +1,19 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Q_33_Find_Longest_Word_in_Sentence {
+    public static void main(String args[]){
+       String GivenSentence="I am Software Development Engineer";
+
+        String myResult  = Arrays.stream(GivenSentence.split(" "))   // converting here cannot direclty use stream
+        .max(Comparator.comparingInt(String::length))
+        .orElse(null);
+
+        System.out.println("Longest Word in Sentence is : \n"+myResult);
+
+        
+    }
+    
+}
