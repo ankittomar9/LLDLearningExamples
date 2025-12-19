60aac96097276ffaa7069b669c98003a74e18436
tomar <tomarankit78@gmail.com>
2025-12-19 19:31:34 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_32_SkipAndLimitElements.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_32_SkipAndLimitElements.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Q_32_SkipAndLimitElements {
     // Skip and Limit the elements in a given List
     public static void main(String args[]){
-        List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+    List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
         
         List<Integer> skipElementsResult = numbers.stream()
                 .skip(2)
