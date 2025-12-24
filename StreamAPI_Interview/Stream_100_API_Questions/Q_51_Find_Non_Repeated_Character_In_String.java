c545523efa450d7c02d56f2002ac01c2e9181baf
tomar <tomarankit78@gmail.com>
2025-12-24 21:44:57 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_51_Find_Non_Repeated_Character_In_String.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_51_Find_Non_Repeated_Character_In_String.java
@@ -1,5 +1,25 @@
 package StreamAPI_Interview.Stream_100_API_Questions;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Q_51_Find_Non_Repeated_Character_In_String {
+    public static void main(String args[]){
+        String input="java articles are awesome";
+
+        Character result;
+        result = input.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
+
+                System.out.println("first non-repeated character : \n"+result);
+
+    }
     
 }
