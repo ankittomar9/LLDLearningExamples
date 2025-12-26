15cbac7c2e1dfe58242d5df4927222b1fce2ab86
tomar <tomarankit78@gmail.com>
2025-12-26 22:27:17 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_65_Find_second_smallest_array_element.java
@@ -0,0 +1,20 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+
+public class Q_65_Find_second_smallest_array_element {
+    public static void main(String args[]){
+        int numbers[]={5,2,3,8,2,9,1};
+        
+        int second_smallest=Arrays.stream(numbers)
+        .distinct()
+        .sorted()
+        .skip(1)
+        .findFirst()
+        .orElseThrow();
+
+        System.out.println("Second smallest element is \n"+second_smallest);
+
+
+    }
+}
