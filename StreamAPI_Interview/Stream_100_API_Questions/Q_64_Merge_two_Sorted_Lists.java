a33fffe82b5dc347044a0fb06bb24af35aac0d36
tomar <tomarankit78@gmail.com>
2025-12-26 22:16:08 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_64_Merge_two_Sorted_Lists.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_64_Merge_two_Sorted_Lists.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 public class Q_64_Merge_two_Sorted_Lists {
     public static void main(String args[]){
+        //64. Merge two sorted Lists into a single sorted Stream
         List<Integer> list1=Arrays.asList(1,3,5,7);
         List<Integer> list2=Arrays.asList(2,4,6,8);
            System.out.println("original List1 : \n"+list1);
