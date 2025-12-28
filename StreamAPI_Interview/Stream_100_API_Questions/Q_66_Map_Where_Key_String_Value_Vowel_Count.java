8b785a7fc64de3ccecd15cf0288a743ad40db753
tomar <tomarankit78@gmail.com>
2025-12-28 19:01:20 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_66_Map_Where_Key_String_Value_Vowel_Count.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_66_Map_Where_Key_String_Value_Vowel_Count.java
@@ -8,12 +8,13 @@ import java.util.stream.Collectors;
 public class Q_66_Map_Where_Key_String_Value_Vowel_Count {
     //66. Create a Map where the Key is the String and Value is its Vowel Count
     public static void main(String args[]){
-        List<String> words=Arrays.asList("Apple","Java","Stream","Interview");
+    List<String> words=Arrays.asList("Apple","Java","Stream","Interview");
 
-        Map<String ,Long> result=words.stream()
-        .collect(Collectors.toMap(word->word ,word->word.toLowerCase().chars().filter(
-            c->"aeiou".indexOf(c)!=-1).count()));
+     Map<String ,Long> result=words.stream()
+     .collect(Collectors.toMap
+        (word->word ,word->word.toLowerCase().chars().filter(
+         c->"aeiou".indexOf(c)!=-1).count()));
 
-            System.out.println("Count of vowels in Each words : \n"+result);
+     System.out.println("Count of vowels in Each words : \n"+result);
     }
 }
