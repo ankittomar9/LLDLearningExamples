fb1d7fb5fabcd284123041f149457ae8a6903f39
tomar <tomarankit78@gmail.com>
2025-12-29 22:25:04 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_75_Calculate_ProductOfAllNumbers.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_75_Calculate_ProductOfAllNumbers.java
@@ -1,5 +1,15 @@
 package StreamAPI_Interview.Stream_100_API_Questions;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Q_75_Calculate_ProductOfAllNumbers {
+    public static void main(String args[]){
+        List<Integer> numbers=Arrays.asList(1,2,3,4,5);
+        int product =numbers.stream()
+        .reduce(1, (a,b)->a*b);
+
+        System.out.println("Product of All numbers in List is \n"+product);
+    }
     
 }
