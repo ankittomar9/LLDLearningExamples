0d9263b7098685c3d7060569d2c3b46d8efebc74
tomar <tomarankit78@gmail.com>
2025-12-30 00:16:22 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_78_Remove_element_in_a_list.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_78_Remove_element_in_a_list.java
@@ -1,5 +1,21 @@
 package StreamAPI_Interview.Stream_100_API_Questions;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Q_78_Remove_element_in_a_list {
+    public static void main(String args[]){
+        List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9);
+        System.out.println("Original List \n"+ list);
+        int elementToRemove=7;
+
+        List<Integer> resultList=list.stream()
+        .filter(i->i != elementToRemove)
+        .collect(Collectors.toList());
+
+        System.out.println("\nList after removed element : "+elementToRemove + "\n " +resultList);
+
+    }
     
 }
