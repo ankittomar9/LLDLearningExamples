d3f5c0aa9a2b19084802a9a2b8962565d9828454
tomar <tomarankit78@gmail.com>
2025-12-31 00:01:33 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_81_Employee_data_Count_Employees_each_department.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_81_Employee_data_Count_Employees_each_department.java
@@ -8,7 +8,15 @@ public class Q_81_Employee_data_Count_Employees_each_department {
         String Department,int yearOfJoining,double salary){}
     public static void main(String args[]){
         List<Employee> employeeList =Arrays.asList(
-            new Employee(111,"Ankit Singh",27,"Male","Product Development",2011,50000.0)
+    new Employee(111,"Ankit Singh",27,"Male","Product Development",2011,50000.0),
+    new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0),
+    new Employee(122, "Paul Niksui", 25, "Male", "Sales", 2015, 13500.0),
+    new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0),
+    new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0),
+    new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0),
+    new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0),
+    new Employee(199, "Amelia Zoe", 24, "Female", "Sales", 2016, 11500.0)
+       
         );
 
     }
