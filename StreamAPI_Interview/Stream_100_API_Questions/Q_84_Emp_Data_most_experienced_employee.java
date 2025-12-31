82ff054a2cd9c883e6ce603b0f75e99c5f69d8d3
tomar <tomarankit78@gmail.com>
2025-12-31 22:48:37 +0530

    New Stream API in Java  examples added

--- StreamAPI_Interview/Stream_100_API_Questions/Q_84_Emp_Data_most_experienced_employee.java
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_84_Emp_Data_most_experienced_employee.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class Q_83_Emp_Data_Youngest_Male_Employee {
+public class Q_84_Emp_Data_most_experienced_employee {
     public record Employee(int id,String name,int age,String gender,
         String Department,int yearOfJoining,double salary){}
     public static void main(String args[]){
         List<Employee> employeeList =Arrays.asList(
-    new Employee(111,"Ankit Singh",27,"Male","Product Development",2011,50000.0),
+    new Employee(111,"Ankit Singh",27,"Male","Product Development",2013,50000.0),
     new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0),
     new Employee(122, "Paul Niksui", 25, "Male", "Sales", 2015, 13500.0),
     new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0),
@@ -17,15 +17,14 @@ public class Q_83_Emp_Data_Youngest_Male_Employee {
     new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0),
     new Employee(199, "Amelia Zoe", 24, "Female", "Sales", 2016, 11500.0));
 
-            //83. Find the youngest  employee in the Product Development department
-            Employee youngest=employeeList.stream()
-            .filter(n->"Product Development".equals(n.Department()))
-            .min(Comparator.comparingInt(Employee::age))
+            //84. Find the most experienced employee (Earliest Year of Joining)
+            Employee seniorMost=employeeList.stream()
+            .min(Comparator.comparingInt(Employee::yearOfJoining))
             .orElseThrow();
        
 
-            System.out.println("youngest employee in  Product Development department : \n ");    
-            System.out.println(youngest);
+            System.out.println("most experienced employee : \n ");    
+            System.out.println(seniorMost);
 
     }
     
