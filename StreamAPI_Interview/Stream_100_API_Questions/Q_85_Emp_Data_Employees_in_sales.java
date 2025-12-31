25e916649aa7c45cd66df1ec9e1ce96450e0c284
tomar <tomarankit78@gmail.com>
2025-12-31 22:47:00 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_85_Emp_Data_Employees_in_sales.java
@@ -0,0 +1,32 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Q_83_Emp_Data_Youngest_Male_Employee {
+    public record Employee(int id,String name,int age,String gender,
+        String Department,int yearOfJoining,double salary){}
+    public static void main(String args[]){
+        List<Employee> employeeList =Arrays.asList(
+    new Employee(111,"Ankit Singh",27,"Male","Product Development",2011,50000.0),
+    new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0),
+    new Employee(122, "Paul Niksui", 25, "Male", "Sales", 2015, 13500.0),
+    new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0),
+    new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0),
+    new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0),
+    new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0),
+    new Employee(199, "Amelia Zoe", 24, "Female", "Sales", 2016, 11500.0));
+
+            //83. Find the youngest  employee in the Product Development department
+            Employee youngest=employeeList.stream()
+            .filter(n->"Product Development".equals(n.Department()))
+            .min(Comparator.comparingInt(Employee::age))
+            .orElseThrow();
+       
+
+            System.out.println("youngest employee in  Product Development department : \n ");    
+            System.out.println(youngest);
+
+    }
+    
+}
