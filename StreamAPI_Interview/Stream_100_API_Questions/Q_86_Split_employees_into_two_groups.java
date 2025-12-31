ce635397c4b2f6cdb6779ec1995f70824f16cd60
tomar <tomarankit78@gmail.com>
2026-01-01 00:06:32 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_86_Split_employees_into_two_groups.java
@@ -0,0 +1,31 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Q_86_Split_employees_into_two_groups {
+    public record Employee(int id,String name,int age,String gender,
+     String department,int yearOfJoining,double salary){}
+    public static void main(String args[]){    
+
+        List<Employee> employeeList =Arrays.asList(
+    new Employee(111,"Ankit Singh",27,"Male","Product Development",2011,50000.0),
+    new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0),
+    new Employee(122, "Paul Niksui", 25, "Male", "Sales", 2015, 13500.0),
+    new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0),
+    new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0),
+    new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0),
+    new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0),
+    new Employee(199, "Amelia Zoe", 24, "Female", "Sales", 2016, 11500.0));
+
+       //     86. Split employees into two groups: Age > 25 and Age <= 25
+            Map<Boolean,List<Employee>> partitionByAge=employeeList.stream()
+            .collect(Collectors.partitioningBy(e->e.age()>30));
+
+            System.out.println(partitionByAge);
+    
+    }
+    
+}
