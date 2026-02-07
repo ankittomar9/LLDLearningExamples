20912e317da62dd21dc789370b7fae276d8cef7b
tomar <tomarankit78@gmail.com>
2026-02-07 22:33:48 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_97_Count_Employees_in_Each_Department.java
@@ -0,0 +1,32 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Q_97_Count_Employees_in_Each_Department {
+    public record Employee(int id,String name,int age,String gender,
+     String department,int yearOfJoining,double salary){}
+    public static void main(String args[]){    
+
+          List<Employee> employeeList = Arrays.asList(
+    new Employee(111, "Ankit Singh", 27, "Male", "Product Development", 2011, 50000.0),
+    new Employee(112, "Pam Beesly", 24, "Female", "HR", 2011, 25000.0),
+    new Employee(122, "Jim Halpert", 25, "Male", "Sales", 2015, 23500.0),
+    new Employee(133, "Creed Bratton", 29, "Male", "Infrastructure", 2012, 18000.0), 
+    new Employee(144, "Dwight Schrute", 28, "Male", "Product Development", 2014, 32500.0),
+    new Employee(155, "Angela Martin", 27, "Female", "HR", 2013, 22700.0),
+    new Employee(188, "Michael Scott", 31, "Male", "Product Development", 2015, 34500.0),
+    new Employee(199, "Phyllis Vance", 24, "Female", "Sales", 2016, 11500.0)
+);
+       // 97 Count Employees in Each Department, Sorted by Count (Descending)
+          List<Map.Entry<String,Long>> sortedDeptCounts=employeeList.stream()
+          .collect(Collectors.groupingBy(Employee::department,Collectors.counting()))
+          .entrySet().stream()
+          .sorted(Map.Entry.<String,Long> comparingByValue().reversed())
+          .collect(Collectors.toList());
+
+          System.out.println("Sorted  Dept Counts: "+ sortedDeptCounts);
+    }
+  }
