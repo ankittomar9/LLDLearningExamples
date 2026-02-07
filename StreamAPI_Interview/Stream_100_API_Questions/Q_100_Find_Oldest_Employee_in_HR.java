906397f29a76d6ce7a9998b18229c2f99e1c457c
tomar <tomarankit78@gmail.com>
2026-02-07 22:34:01 +0530

    New Stream API in Java  examples added

---
+++ StreamAPI_Interview/Stream_100_API_Questions/Q_100_Find_Oldest_Employee_in_HR.java
@@ -0,0 +1,30 @@
+package StreamAPI_Interview.Stream_100_API_Questions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Q_100_Find_Oldest_Employee_in_HR {
+    public record Employee(int id,String name,int age,String gender,
+     String department,int yearOfJoining,double salary){}
+    public static void main(String args[]){    
+
+          List<Employee> employeeList = Arrays.asList(
+    new Employee(111, "Ankit Singh", 27, "Male", "Product Development", 2011, 50000.0),
+    new Employee(112, "Pam Beesly", 24, "Female", "HR", 2011, 25000.0),
+    new Employee(122, "Jim Halpert", 25, "Male", "Sales", 2015, 23500.0),
+    new Employee(133, "Creed Bratton", 29, "Male", "Infrastructure", 2012, 18000.0), 
+    new Employee(144, "Dwight Schrute", 28, "Male", "Product Development", 2014, 32500.0),
+    new Employee(155, "Angela Martin", 27, "Female", "HR", 2013, 22700.0),
+    new Employee(188, "Michael Scott", 31, "Male", "Product Development", 2015, 34500.0),
+    new Employee(199, "Phyllis Vance", 24, "Female", "Sales", 2016, 11500.0)
+);
+       // 100. Find the Oldest Employee in the "HR" Department
+         Employee oldestInHR=employeeList.stream()
+         .filter(e->"HR".equals(e.department()))
+         .max(Comparator.comparing(Employee::age))
+         .orElse(null);
+
+          System.out.println("Oldest in HR: " + oldestInHR);
+    }
+  }
