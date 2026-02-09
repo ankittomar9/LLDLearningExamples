91100051849472cd5717677197d50bb96fb9387b
tomar <tomarankit78@gmail.com>
2026-02-09 22:43:21 +0530

    New Stream API in Java  examples added

--- JavaCollections/MapDifferences.java
+++ JavaCollections/MapDifferences.java
@@ -1,29 +1,31 @@
-package LLDLearningExamples.JavaCollections;
+package JavaCollections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 public class MapDifferences {
-    
     public static void main(String[] args) {
         // 1. HashMap: No guaranteed order
         Map<String, Integer> hashMap = new HashMap<>();
         hashMap.put("Apple", 1);
-        hashMap.put("Banana", 2);
-        hashMap.put("Cherry", 3);
-        System.out.println("HashMap: " + hashMap); // Output order is unpredictable (e.g., {Banana=2, Apple=1, Cherry=3})
+         hashMap.put("Banana", 2);
+          hashMap.put("Cherry", 3);
+        System.out.println("HashMap: \n" + hashMap); 
+        // Output order is unpredictable (e.g., {Banana=2, Apple=1, Cherry=3})
 // 2. LinkedHashMap: Insertion order is maintained
         Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
-        linkedHashMap.put("Apple", 1);
-        linkedHashMap.put("Banana", 2);
-        linkedHashMap.put("Cherry", 3);
-        System.out.println("LinkedHashMap: " + linkedHashMap); // Output: {Apple=1, Banana=2, Cherry=3}
+         linkedHashMap.put("Cherry", 3);
+          linkedHashMap.put("Apple", 1);
+          linkedHashMap.put("Banana", 2);
+              System.out.println("LinkedHashMap: \n" + linkedHashMap); 
+        // Output: {Apple=1, Banana=2, Cherry=3}
 // 3. TreeMap: Sorted order by key (A-Z for String)
         Map<String, Integer> treeMap = new TreeMap<>();
         treeMap.put("Apple", 1);
         treeMap.put("Banana", 2);
         treeMap.put("Cherry", 3);
-        System.out.println("TreeMap: " + treeMap); // Output: {Apple=1, Banana=2, Cherry=3}
+        System.out.println("TreeMap: \n" + treeMap); 
+        // Output: {Apple=1, Banana=2, Cherry=3}
     }
 }
 
